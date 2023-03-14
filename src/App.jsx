d911dd87adfce3e4dd345c6ad65a7ccb7bd10169
Teodor <theodor.rotaru@gmail.com>
2023-03-14 23:27:57 +0200

    Version 1 restored

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
 
-  return <div className='App'>App version 3</div>
+  return <div className='App'>App</div>
 }
 
 export default App
